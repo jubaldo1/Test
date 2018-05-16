298b839a912fcd7dfc078f52195376edf69e9346
Jessica May Ubaldo <jubaldo@csumb.edu>
2018-05-16 09:10:36 +0000

    Doing tests, adding some stuff

--- Test3/practF1/index.php
+++ Test3/practF1/index.php
@@ -9,11 +9,29 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" src="css/styles.css">
         <script type='text/javascript'>
-            
-        
+            /* global $ */
+            $(document).ready(function () {
+                $("body").append("<h3>Test Title</h3>");
+                
+                // button to dynamically append test to the page
+                $("#testBtn").click(function () {
+                    $("#testBtnResult").append("Test");
+                });
+                
+                // button to clear child elements from the page
+                $("#clearTestResults").click(function () {
+                    $("#testBtnResult").empty();
+                });
+            });
         </script>
     </head>
     <body>
+        <!--Don't add this to a form, or else it will submit it-->
+        <button id="testBtn">Test</button>
+        <button id="clearTestResults">Remove</button>
+        
+        <div id="testBtnResult"></div>
     </body>
 </html>
